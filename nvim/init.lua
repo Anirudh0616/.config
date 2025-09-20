929442287f4b5b7740e477c26295957467c5e4ce
Anirudh0616 <anirudh.b.0616@gmail.com>
2025-09-20 14:20:15 +0530

    New Markdown Reader plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,6 +69,15 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+	{
 		"mbbill/undotree",
 		cmd = "UndotreeToggle",
 		config = function()
